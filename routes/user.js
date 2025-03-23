d7a143a6388eddf9efb9d4e5e39481a301288ea8
SayambarKaran <karan.subhashsayambar@quadbtech.com>
2025-03-23 20:12:00 +0530

    Initial commit

---
+++ routes/user.js
@@ -0,0 +1,27 @@
+const router = require("express").Router();
+
+const userController = require("../controllers/userController");
+const authController = require("../controllers/authController");
+
+router.post(
+  "/generate-zego-token",
+  authController.protect,
+  userController.generateZegoToken
+);
+router.get(
+  "/get-call-logs",
+  authController.protect,
+  userController.getCallLogs
+);
+router.get("/get-me", authController.protect, userController.getMe);
+router.patch("/update-me", authController.protect, userController.updateMe);
+router.get(
+  "/get-all-verified-users",
+  authController.protect,
+  userController.getAllVerifiedUsers
+);
+router.get("/get-users", authController.protect, userController.getUsers);
+router.get("/get-requests", authController.protect, userController.getRequests);
+router.get("/get-friends", authController.protect, userController.getFriends);
+
+module.exports = router;
